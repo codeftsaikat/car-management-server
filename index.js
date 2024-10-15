42fae3443b2a0cec8dd7b5bb72b52d05a0d2b155
codeftsaikat <soikatdey18@gmail.com>
2024-10-15 11:06:38 -0700

    remove localhost pathname from this project

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+      // "http://localhost:5173",
       "cars-management-3f382.web.app",
       "cars-management-3f382.firebaseapp.com"
     ],
